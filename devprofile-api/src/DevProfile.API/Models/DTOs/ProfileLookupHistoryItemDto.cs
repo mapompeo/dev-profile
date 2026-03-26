0d2ceb23eef5b0cfd04e528e289319c379b73067
Matheus Pompeo <matheuspompeo13@gmail.com>
2026-03-26 16:56:56 -0300

    feat(web): inicializa a aplicação Angular
    Projeto Angular com roteamento e proxy para a API, mais a limpeza do código
    morto que sobrou do histórico de perfil.

--- devprofile-api/src/DevProfile.API/Models/DTOs/ProfileLookupHistoryItemDto.cs
+++
@@ -1,8 +0,0 @@
-namespace DevProfile.API.Models.DTOs;
-
-public sealed class ProfileLookupHistoryItemDto
-{
-    public required string Username { get; init; }
-    public required string Endpoint { get; init; }
-    public DateTime QueriedAtUtc { get; init; }
-}
